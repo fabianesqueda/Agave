8d12c861f042a87893c461c7899dad25451ee24f
Fabian Esqueda <fabianesqueda@Fabians-MacBook-Pro.local>
2018-01-24 18:01:49 +0200

    Header Files

--- src/dsp/Filters.hpp
+++ src/dsp/Filters.hpp
@@ -9,27 +9,30 @@
 // TODO: 
 // 		ADD MORE STANDARD FILTERS
 #pragma once
+#ifndef RCFILTER_H
+#define RCFILTER_H
 
 class RCFilter {
-
 // THIS CLASS IMPLEMENTES AN LTI FIRST-ORDER LOWPASS FILTER DERIVED FROM THE TRANSFER FUNCTION 
 // OF A SIMPLE PASSIVE RC FILTER. THE FILTER IS PARAMETRIZED BY SETTING THE CUTOFF FREQUENCY IN HZ
 // I.E. fc = 1 / (2*pi*R*C)
-
+// 
+// Usage example:
+// 	RCFilter filter(100.0f,44100.0f);
+// 	filter.process(x);
+// 	filter.getLowpassOutput();
+// 
 private:
-
-	// Default parameters. Use constructor to overwrite.
 	float sampleRate = 44100.0;
 	float fc = 1.0e3;
-	
 	float previousInput = 0.0;
 	float wc = 0.0;
 	float lowpassOutput = 0.0;
 	float highpassOutput = 0.0;
 
-
 public:
 
+	// Constructor/Destrcutor
 	RCFilter() {}
 	RCFilter(float cutoffFrequency, float SR) { 
 		fc = cutoffFrequency;
@@ -38,38 +41,15 @@ public:
 	}
 	~RCFilter() {}
 
-	void setSampleRate(float SR) {
-		sampleRate = SR;
-		setCutoff();
-	}
-
-	void setCutoff() {
-		float wa = 2.0f*M_PI*fc; // analog cutoff freq
-		wc = 2.0f*std::atan(0.5f*wa/sampleRate)*sampleRate;	// digital cutoff freq
-	}
-
-	void process(float input) {
-
-		float alpha = 2.0f*sampleRate/wc;
-
-		// Compute filter output
-		lowpassOutput = ( (alpha - 1.0f)*lowpassOutput + input + previousInput ) / (1.0f + alpha);
-		highpassOutput = input - lowpassOutput;
-
-		// Update State
-		previousInput = input;
-
-	}
-
-	float getLowpassOutput() {
-		return lowpassOutput;
-	}
-
-	float getHighpassOutput() {
-		return highpassOutput;
-	}
+	// Other member functions
+	void setSampleRate(float SR);
+	void setCutoff();
+	void process(float input);
+	float getLowpassOutput() { return lowpassOutput; }
+	float getHighpassOutput() { return highpassOutput; }
 };
 
+#endif
 
 class DCBlocker {
 
@@ -91,6 +71,7 @@ private:
 
 public:
 
+	// Constructor/Destrcutor
 	DCBlocker() { setPole(); }
 	DCBlocker(float cutoffFrequency, float SR) { 
 		fc = cutoffFrequency;
@@ -99,28 +80,11 @@ public:
 	}
 	~DCBlocker() {}
 
-	void setSampleRate(float SR) {
-		sampleRate = SR;
-		setPole();
-	}
-
-	void setPole() {
-		p = std::tan(0.25f*M_PI - M_PI*fc/sampleRate); // Filter pole
-	}
-
-	void process(float input) {
-
-		output = 0.5f*(1.0 + p) * ( input - xState + p*yState ); 
-
-		// Update State
-		xState = input;
-		yState = output;
-
-	}
-
-	float getFilteredOutput() {
-		return output;
-	}
+	// Other member functions
+	void setSampleRate(float SR);
+	void setPole();
+	void process(float input);
+	float getFilteredOutput() { return output; }
 
 };
 
